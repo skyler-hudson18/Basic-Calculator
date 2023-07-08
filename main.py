11c9982ea0e61478b1bc0a4354de47caf829c79f
skyler-hudson18 <humbleaddictionz43@gmail.com>
2023-07-08 17:49:47 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Basic Math Calculator
+number1 = 10
+number2 = 10
+
+add = number1 + number2
+subtract = number1 - number2
+multiply = number1 * number2
+divide = number1/number2
+squared = number1**number2
+
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+print(squared)
