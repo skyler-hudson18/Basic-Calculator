bb4395fc036a8e8759b3d0e33efea6786e696463
skyler-hudson18 <139017425+skyler-hudson18@users.noreply.github.com>
2023-07-08 18:15:54 -0500

    Update main.py
    Added in some math functions as well as imported the math module

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Basic Math Calculator
+import math
+
 number1 = 10
 number2 = 10
 
@@ -6,10 +8,14 @@ add = number1 + number2
 subtract = number1 - number2
 multiply = number1 * number2
 divide = number1/number2
-squared = number1**number2
+power = pow(number1, number2)
+square_root1 = math.sqrt(number1)
+square_root2 = math.sqrt(number2)
 
 print(add)
 print(subtract)
 print(multiply)
 print(divide)
-print(squared)
+print(power)
+print(square_root1)
+print(square_root2)
